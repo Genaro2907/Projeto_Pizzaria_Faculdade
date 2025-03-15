0269590fa72fbd589e00c68127581e71226dcc2f
Gabriel Genaro <g.genaro.contato@gmail.com>
2025-03-14 21:09:56 -0300

    Teste 01

--- Projeto_Pizzaria_Faculdade/src/projeto_pizzaria_faculdade/Program.java
+++ Projeto_Pizzaria_Faculdade/src/projeto_pizzaria_faculdade/Program.java
@@ -4,6 +4,14 @@
  */
 package projeto_pizzaria_faculdade;
 
+import java.util.ArrayList;
+
+import projeto_pizzaria_faculdade.Util.Cliente;
+import projeto_pizzaria_faculdade.Util.Pedido;
+import projeto_pizzaria_faculdade.Util.items.Bebida;
+import projeto_pizzaria_faculdade.Util.items.Pizza;
+import projeto_pizzaria_faculdade.Util.items.StatusPedido;
+
 /**
  *
  * @author Gabriel
@@ -14,7 +22,24 @@ public class Program {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    	
+    	        Cliente cliente = new Cliente("João Silva", "Rua das Flores, 123");
+    	        
+    	        Pedido pedido = new Pedido(cliente);
+    	        
+    	        pedido.adicionarItem(new Pizza("Margherita", 25.0, "Média", new ArrayList<>()));
+    	        pedido.adicionarItem(new Bebida("Coca-Cola", 8.0, "500ml", true));
+    	        
+    	        pedido.setStatus(StatusPedido.EM_PREPARO);
+    	        
+    	        System.out.println("========== DETALHES DO PEDIDO ==========");
+    	        System.out.println(pedido.getDetalhesPedido());
+    	        System.out.println("========================================");
+    	        
+    	        System.out.println("\nTodos os pedidos registrados:");
+    	        for (Pedido p : Pedido.getTodosPedidos()) {
+    	            System.out.println("Pedido #" + p.getIdPedido() + " - " + p.getCliente().getNomeCliente());
+    	   }
     }
-    
+  
 }
